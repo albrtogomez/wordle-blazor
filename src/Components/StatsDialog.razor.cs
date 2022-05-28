3fd3a876d3f2025d2b098fe5f2b93ce63ffb2b59
Alberto Gómez <albrto.gomez@outlook.com>
2022-05-28 02:09:35 +0200

    Fixed bug when loading statistics if there is not data in browsers local storage

--- src/Components/StatsDialog.razor.cs
+++ src/Components/StatsDialog.razor.cs
@@ -4,24 +4,27 @@ namespace WordleBlazor.Components
 {
     public partial class StatsDialog
     {
-        private Stats stats = null!;
+        private Stats stats = new();
 
         protected override async Task OnInitializedAsync()
         {
             stats = await localStorage.GetItemAsync<Stats>(nameof(Stats));
-
-            if (stats == null)
-                stats = new Stats();
         }
 
         private string GetGamesWonPercent()
         {
-            return (stats.GamesWon * 100 / @stats.GamesPlayed).ToString();
+            if (stats.GamesPlayed > 0)
+                return (stats.GamesWon * 100 / stats.GamesPlayed).ToString();
+            else
+                return "0";
         }
 
         private string GetGameResultDistributionPercent(int keyRow)
         {
-            return (stats.GamesResultDistribution[keyRow] * 100 / @stats.GamesPlayed).ToString();
+            if (stats.GamesPlayed > 0)
+                return (stats.GamesResultDistribution[keyRow] * 100 / stats.GamesPlayed).ToString();
+            else
+                return "0";
         }
     }
 }
